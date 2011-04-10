6f5a68420f7a6526bc41d4f17e5c27486571f9fb
sergiosouzalima <sergiosouzalima@gmail.com>
2011-04-10 17:16:31 -0300

    commit before add model named Local

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ActiveRecord::Base
   has_many :datafiles, :dependent => :destroy
   has_many :contactkinds, :dependent => :destroy
   
+  after_create :create_contact_kinds
+  
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable, :lockable and :timeoutable
   devise :database_authenticatable, :registerable,
@@ -13,4 +15,12 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
+  
+  private
+    def create_contact_kinds
+      %w(fax site site2 email email2 phone phone2 mobile mobile2 blog linkedin facebook twitter).each do |name|
+        Contactkind.create( :name => name, :user_id => self.id )
+      end
+    end
+    
 end
