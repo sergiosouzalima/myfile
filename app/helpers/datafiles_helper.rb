cba28394d0f6bd3eb8610700b3692ac1ecde60ba
sergiosouzalima <sergiosouzalima@gmail.com>
2011-05-22 22:13:54 -0300

    Finished the creating-local v8

--- app/helpers/datafiles_helper.rb
+++ app/helpers/datafiles_helper.rb
@@ -4,4 +4,12 @@ module DatafilesHelper
     ! (f.object.nil? || f.object.new_record?)
   end
   
+  def show_first_contact_description( datafile )
+    if datafile.locals.any?
+      if datafile.locals.first.contacts.any?
+        datafile.locals.first.contacts.first.description
+      end
+    end
+  end
+  
 end
